0ddc92e97ff005a70fc538422fdb0ce904200d15
brendonsmsteele <brendonsmsteele@gmail.com>
2019-06-05 15:26:37 -0400

    Moved pages to StreamingAssets and created async load compatible with StreamingAssets.

--- Assets/Bs.Shell/CodeGeneration/Generated/MainMenu/MainMenuControllerData.cs
+++ Assets/Bs.Shell/CodeGeneration/Generated/MainMenu/MainMenuControllerData.cs
@@ -5,6 +5,9 @@ namespace Bs.Shell.MainMenu
     [CreateAssetMenu(fileName = nameof(MainMenuControllerData), menuName = "Bs.Shell/Controllers/" + nameof(MainMenuControllerData))]
     public class MainMenuControllerData : ControllerData
     {
+        public string Message;
+        public Sprite Sprite;
+
         public static MainMenuControllerData Create()
         {
             var data = ScriptableObject.CreateInstance<MainMenuControllerData>();
--- Assets/Bs.Shell/Scripts/Shell/Editor/NavigationPageGenerator.cs
+++ Assets/Bs.Shell/Scripts/Shell/Editor/NavigationPageGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEditor;
@@ -9,45 +10,124 @@ namespace Bs.Shell.Navigation
     [CreateAssetMenu(fileName =nameof(NavigationPageGenerator), menuName = "Bs.Shell/Navigation/"+ nameof(NavigationPageGenerator))]
     public class NavigationPageGenerator : ScriptableObject
     {
-        string outputPath = "NavigationPages";
+        [SerializeField] string pathToControllers = "NavigationMap/Controllers";
+        [SerializeField] string pathToOutputFolder = "NavigationMap/Pages";
 
         public void Generate()
         {
-            CreateAllPagesFromControllersResources();
+            GenerateAllPagesFromControllers();
         }
 
-        private void CreateAllPagesFromControllersResources()
+        private void GenerateAllPagesFromControllers()
         {
-            var currentDirectory = GetPathOfParent();
-            var directories = Directory.GetDirectories(currentDirectory);
+            Watchdog.Stop();
+            Watchdog.Start(CreateAllPagesRoutine());
+        }
+
+        CoroutineWatchdog Watchdog = new CoroutineWatchdog();
+        private IEnumerator CreateAllPagesRoutine()
+        {
+            var rootDirectory = Path.Combine(Application.streamingAssetsPath, pathToControllers);
+            var directories = Directory.GetDirectories(rootDirectory);
             for (int i = 0; i < directories.Length; i++)
             {
                 var directory = directories[i];
-                var objects = LoadAllObjectsFromPath(directory);
-                //var clones = CloneObjects(objects); //  Clone so we prevent crazy overwriting data issues.
+                //var shortDirectory = directory.Substring(Application.streamingAssetsPath.Count());
+                var waitForObjects = new GetAllObjectsFromFolderAsync(directory);
+                yield return waitForObjects;
+                var objects = waitForObjects.Objects;
                 var controllers = objects.Cast<ControllerData>().ToArray();
-                var navPage = CreateNavigationPage(controllers);
-                AssetDatabase.CreateAsset(navPage, currentDirectory+outputPath);
+                var name = directory.Split('\\').LastOrDefault();
+                var navPage = CreateNavigationPage(controllers, name);
+                CreateNavigationPageAsset(navPage);
+            }
+            AssetDatabase.SaveAssets();
+        }
+
+        private class GetAllObjectsFromFolderAsync : CustomYieldInstruction
+        {
+            public override bool keepWaiting
+            {
+                get
+                {
+                    return Objects == null;
+                }
+            }
+
+            public List<Object> Objects;
+
+            public GetAllObjectsFromFolderAsync(string path)
+            {
+                RunCoroutine.Instance.StartCoroutine(Routine(path));
+            }
+
+            private IEnumerator Routine(string path)
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(path);
+                FileInfo[] allFiles = directoryInfo.GetFiles("*.*");
+                List<Object> loadingObjects = new List<Object>();
+                foreach (var file in allFiles)
+                {
+                    if (file.Name.Contains("meta"))
+                        continue;
+
+                    var getObject = new GetObjectFromPathAsync(file);
+                    yield return getObject;
+                    loadingObjects.Add(getObject.Object);
+                }
+                Objects = loadingObjects;
             }
         }
 
-        private List<Object> LoadAllObjectsFromPath(string path)
+        private class GetObjectFromPathAsync : CustomYieldInstruction
         {
-            var controllersFromPath = Resources.LoadAll(path).ToList();
-            return controllersFromPath;
+            public override bool keepWaiting { get { return Object == null; } }
+
+            public Object Object = null;
+
+            public GetObjectFromPathAsync(FileInfo fileInfo)
+            {
+                RunCoroutine.Instance.StartCoroutine(Routine(fileInfo));
+            }
+
+            private IEnumerator Routine(FileInfo fileInfo)
+            {
+                string fileWithoutExtension = Path.GetFileNameWithoutExtension(fileInfo.ToString());
+                string[] nameData = fileWithoutExtension.Split(" "[0]);
+                //3
+                string tempPlayerName = "";
+                int i = 0;
+                foreach (string stringFromFileName in nameData)
+                {
+                    if (i != 0)
+                    {
+                        tempPlayerName = tempPlayerName + stringFromFileName + " ";
+                    }
+                    i++;
+                }
+                //4
+                string wwwPlayerFilePath = "file://" + fileInfo.FullName.ToString();
+                WWW www = new WWW(wwwPlayerFilePath);
+                yield return www;
+                //5
+                Object = (Object)www.Current;
+            }
+
         }
 
-        private NavigationPage CreateNavigationPage(ControllerData[] controllers)
+        private NavigationPage CreateNavigationPage(ControllerData[] controllers, string name)
         {
             var navPage = NavigationPage.Create(controllers);
+            navPage.name = name;
             return navPage;
         }
 
-        protected string GetPathOfParent()
+        private NavigationPage CreateNavigationPageAsset(NavigationPage navPage)
         {
-            string thisPath = AssetDatabase.GetAssetPath(this);
-            string parentPath = Directory.GetParent(thisPath).FullName;
-            return parentPath;
+            var outputPath = Application.streamingAssetsPath + pathToOutputFolder + navPage.name + ".asset";
+            AssetDatabase.CreateAsset(navPage, outputPath);
+            return navPage;
         }
+
     }
 }
